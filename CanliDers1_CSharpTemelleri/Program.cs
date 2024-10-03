20054239550e8a11e865e983760bab131402400d
iremyakincankaya <yakincanirem@gmail.com>
2024-10-03 19:31:06 +0300

    Add project files.

---
+++ CanliDers1_CSharpTemelleri/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CanliDers1_CSharpTemelleri
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //cw [TAB]
+            Console.WriteLine("Hello World");
+
+
+            // programı calistirma F5(debug) ya da VTRL+F5(debugsız) yani gercek hayat senaryoyu gösterir
+
+            // bir büyük projede calısıyorsun ve bi yer eksk kaldı unutmak istemiyorsun
+
+            int toplam = 2 + 2;
+
+
+
+        }
+    }
+}
