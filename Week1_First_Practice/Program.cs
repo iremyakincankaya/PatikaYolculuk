b133fcb4e68b6e3019136995cce498014e84461e
iremyakincankaya <yakincanirem@gmail.com>
2024-10-08 09:26:37 +0300

    completed

---
+++ Week1_First_Practice/Program.cs
@@ -0,0 +1,4 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I aim to move forward with solid foundations by making a quick entry into the software world with Patika's expert staff and experienced instructors.");
+Console.ReadKey;
