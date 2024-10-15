002b09c5b3bb8b0064657bf6ebdaf4460a53ee78
iremyakincankaya <yakincanirem@gmail.com>
2024-10-15 11:46:37 +0300

    completed

---
+++ Practice_Constructor/Bebek.cs
@@ -0,0 +1,42 @@
+﻿//using Practice_Constructor;
+
+//Bebek sınıfı için 2 adet constructor tanımlayınız.
+
+//1.si Default Constructor tarzında parametre almayan bir metot.
+
+//2.si Ad ve Soyad parametrelerini alarak newleme işlemi sırasında değerleri propertylere atayan alternatif constructor.
+
+//2 Constructor içinde ortak olarak bebek nesnesi oluşturulduğunda konsol ekranında bir " Ingaaaa " yazısı görülsün ve Doğum Tarihi o an olarak atansın.
+
+//Tanımlama sonrasında her iki constructor ile de birer bebek nesnesi tanımlayarak özelliklerini konsol ekranına yazdırınız.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Constructor
+{
+    public class Bebek
+    {
+        public string _ad;
+        public string _soyad;
+        public DateTime _dogumTarihi;
+
+        public Bebek() 
+        {
+            Console.WriteLine("Ingaaaa");
+            _dogumTarihi = DateTime.Now;
+        }
+
+        public Bebek( string ad, string soyad) 
+        {
+            _ad = ad;
+            _soyad = soyad;
+            Console.WriteLine("Ingaaaa");
+            _dogumTarihi = DateTime.Now;
+        }
+
+    }
+}
---
+++ Practice_Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice_Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Default Constructor ile bebek nesnesi oluşturma
+            Bebek bebek1 = new Bebek();
+            Console.WriteLine($"Ad: {bebek1._ad}, Soyad: {bebek1._soyad}, Doğum Tarihi: {bebek1._dogumTarihi}");
+
+            // Parametreli Constructor ile bebek nesnesi oluşturma
+            Bebek bebek2 = new Bebek("Ali", "Yılmaz");
+            Console.WriteLine($"Ad: {bebek2._ad}, Soyad: {bebek2._soyad}, Doğum Tarihi: {bebek2._dogumTarihi}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
