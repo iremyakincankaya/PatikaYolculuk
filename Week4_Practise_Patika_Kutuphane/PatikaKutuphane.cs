61b2e0bb767dd835a91a580d48f54f9d2ace730b
iremyakincankaya <yakincanirem@gmail.com>
2024-10-15 14:33:21 +0300

    completed

---
+++ Week4_Practise_Patika_Kutuphane/PatikaKutuphane.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_Practise_Patika_Kutuphane
+{
+    //class
+    internal class PatikaKutuphane
+    {
+        //property
+        public string _kitapAd;
+        public string _yazarAd;
+        public string _yazarSoyad;
+        public string _yayinevi;
+        public int _sayfaSayisi;
+        public DateTime _kayitTarihi;
+
+        //default constructor
+        public PatikaKutuphane()
+        {
+            _kayitTarihi = DateTime.Now;
+        }
+        //parametreli constructor
+        public PatikaKutuphane(string kitapAd, string yazarAd, int sayfaSayisi, string yayinevi)
+        {
+            _kitapAd = kitapAd;
+            _yazarAd = yazarAd;
+            _sayfaSayisi = sayfaSayisi;
+            _yayinevi = yayinevi;
+            _kayitTarihi = DateTime.Now;
+        }
+    }
+}
---
+++ Week4_Practise_Patika_Kutuphane/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Week4_Practise_Patika_Kutuphane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Default Constructor ile bebek nesnesi oluşturma
+            PatikaKutuphane kitap1 = new PatikaKutuphane();
+            kitap1._kitapAd = "Adı Aylin";
+            kitap1._yazarAd = "Ayşe";
+            kitap1._yazarSoyad = "Kulin";
+            kitap1._sayfaSayisi = 398;
+            kitap1._yayinevi = "Remzi Kitabevi";
+
+            Console.WriteLine($"Kitap Ad: {kitap1._kitapAd} Yazar: {kitap1._yazarAd} {kitap1._yazarSoyad} Sayfa Sayısı: {kitap1._sayfaSayisi} Yayınevi: {kitap1._yayinevi} Kayıt Tarihi: {kitap1._kayitTarihi}");
+
+
+            // Parametreli Constructor ile bebek nesnesi oluşturma
+            PatikaKutuphane kitap2 = new PatikaKutuphane("Adı Aylin", "Ayşe", 398, "Remzi Kitabevi");
+            kitap2._yazarSoyad = "Kulin";
+            Console.WriteLine($"Kitap Ad: {kitap2._kitapAd} Yazar: {kitap2._yazarAd} {kitap2._yazarSoyad} Sayfa Sayısı: {kitap2._sayfaSayisi} Yayınevi: {kitap2._yayinevi} Kayıt Tarihi: {kitap2._kayitTarihi}");
+
+            Console.ReadKey();
+        }
+    }
+}
