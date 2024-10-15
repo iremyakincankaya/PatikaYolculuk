1d1cea277d482237cb2a97d6fa2b1e84538c37b8
iremyakincankaya <yakincanirem@gmail.com>
2024-10-15 14:37:08 +0300

    completed

--- Week4_Practise_Patika_Kutuphane/Program.cs
+++ Week4_Practise_Patika_Kutuphane/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            // Default Constructor ile bebek nesnesi oluşturma
+            // Default Constructor ile kitap nesnesi oluşturma
             PatikaKutuphane kitap1 = new PatikaKutuphane();
             kitap1._kitapAd = "Adı Aylin";
             kitap1._yazarAd = "Ayşe";
@@ -15,7 +15,7 @@
             Console.WriteLine($"Kitap Ad: {kitap1._kitapAd} Yazar: {kitap1._yazarAd} {kitap1._yazarSoyad} Sayfa Sayısı: {kitap1._sayfaSayisi} Yayınevi: {kitap1._yayinevi} Kayıt Tarihi: {kitap1._kayitTarihi}");
 
 
-            // Parametreli Constructor ile bebek nesnesi oluşturma
+            // Parametreli Constructor ile kitap nesnesi oluşturma
             PatikaKutuphane kitap2 = new PatikaKutuphane("Adı Aylin", "Ayşe", 398, "Remzi Kitabevi");
             kitap2._yazarSoyad = "Kulin";
             Console.WriteLine($"Kitap Ad: {kitap2._kitapAd} Yazar: {kitap2._yazarAd} {kitap2._yazarSoyad} Sayfa Sayısı: {kitap2._sayfaSayisi} Yayınevi: {kitap2._yayinevi} Kayıt Tarihi: {kitap2._kayitTarihi}");
