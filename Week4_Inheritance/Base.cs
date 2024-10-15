545e37b29cf7a9b9a52989b39f26a94801c9a394
iremyakincankaya <yakincanirem@gmail.com>
2024-10-15 23:19:17 +0300

    completed

---
+++ Week4_Inheritance/Base.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Channels;
+
+namespace Week4_Inheritance
+{
+    public abstract class BaseKisi
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void Bilgiler();
+    }
+    //Teacher ve Student sınıfları Base i miras alacak.
+    public class Teacher : BaseKisi
+    {
+        public double MaasBilgisi { get; set; }
+
+        public override void Bilgiler()
+        {
+            Console.WriteLine($"Ögretmen: {FirstName} {LastName} ");
+            Console.WriteLine($"Maaşı: {MaasBilgisi}");
+        }
+    }
+    public class Student : BaseKisi
+    {
+        public int OgrenciNo { get; set; }
+        public override void Bilgiler()
+        {
+            Console.WriteLine($"Ögrenci: {FirstName} {LastName}");
+            Console.WriteLine($"Numarası: {OgrenciNo}");
+        }
+    }
+}
---
+++ Week4_Inheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_Inheritance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student nesnesi tanımlanıyor.
+            Student student1 = new Student();
+            student1.FirstName = "İrem";
+            student1.LastName = "Kaya";
+            student1.OgrenciNo = 12345;
+
+            //Teacher nesnesi tanımlanıyor.
+            Teacher teacher1 = new Teacher();
+            teacher1.FirstName = "Alaattin";
+            teacher1.LastName = "Kaya";
+            teacher1.MaasBilgisi = 100000;
+
+            //Ögretmen ve ögrenci bilgileri metod çagrılarak ekrana yazdırılır.
+            student1.Bilgiler();
+            teacher1.Bilgiler();
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
