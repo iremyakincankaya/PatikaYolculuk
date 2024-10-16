448eb1500e9e9defd9c1b99d281c2c47cba8f155
iremyakincankaya <yakincanirem@gmail.com>
2024-10-16 11:55:01 +0300

    completed

---
+++ Week4_Polymorphism/BaseGeometricShapes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_Polymorphism
+{
+    public class BaseGeometricShapes
+    {
+        //Properties -> Width ve Height
+        public double _width { get; set; }
+        public double _height { get; set; }
+
+        public virtual void AreaCalculatin(double width, double height)
+        {
+            Console.WriteLine($"Area = {width * height}");
+        }
+    }
+    public class Square : BaseGeometricShapes { }
+    public class Rectangle : BaseGeometricShapes { }
+    public class Rectangular : BaseGeometricShapes
+    {
+        public override void AreaCalculatin(double width, double height)
+        {
+            Console.WriteLine($"Area of Rectangular = {width * height / 2}");
+        }
+    }
+}
---
+++ Week4_Polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Week4_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+            square.AreaCalculatin(2, 4);
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.AreaCalculatin(3.5, 2);
+
+            Rectangular rectangular = new Rectangular();
+            rectangular.AreaCalculatin(6.5, 4);
+
+            Console.ReadKey();
+        }
+    }
+}
