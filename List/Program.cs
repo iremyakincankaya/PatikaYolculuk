8f17541dedc1991b07a3babe7a313008d9f97ca6
iremyakincankaya <yakincanirem@gmail.com>
2024-10-26 23:52:36 +0300

    completed

---
+++ List/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel;
+
+//ziyaretci adında liste olusturuldu
+List<string> ziyaretci = new List<string>();
+
+//listeye kişiler eklendi
+ziyaretci.Add("Hadise");
+ziyaretci.Add("Tarkan");
+ziyaretci.Add("Hande Yener");
+ziyaretci.Add("Ajda Pekkan");
+
+//listedeki kişiler ekrana yazdırıldı
+foreach (var item in ziyaretci)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadKey();
