c9b5eec05e989d55c5679dc85fe4ac68b443343a
iremyakincankaya <yakincanirem@gmail.com>
2024-10-27 01:41:05 +0300

    completed

---
+++ IMDBList/Filmler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+
+namespace IMDBList
+{
+    public class Filmler
+    {
+        public string Ad { get; set; }
+        public double IMDB { get; set; }
+
+        public Filmler(string ad, double imdb)
+        {
+            Ad = ad;
+            IMDB = imdb;
+        }
+
+    }
+
+    
+}
---
+++ IMDBList/Program.cs
@@ -0,0 +1,54 @@
+﻿using IMDBList;
+
+List<Filmler> list = new List<Filmler>();
+
+while (true)
+{
+
+    Console.WriteLine("Listeye eklemek istediğiniz filmin adını girin: ");
+    string filmAdi = Console.ReadLine();
+
+    Console.WriteLine("Listeye eklemek istediğiniz filmin imdb puanını girin: ");
+    double imdbPuan = Convert.ToDouble(Console.ReadLine());
+
+    Filmler yeniFilm = new Filmler(filmAdi, imdbPuan);
+
+    list.Add(yeniFilm);
+    Console.WriteLine("Film eklemek istiyor musunuz? (E/H)");
+
+    if (Console.ReadLine().ToLower() != "e")
+    {
+
+        Console.Clear();
+        foreach (Filmler film in list)
+        {
+            Console.WriteLine($"Filmin adı: {film.Ad} IMDB Puanı: {film.IMDB}");
+
+        }
+
+        Console.WriteLine("---------------------------");
+        Console.WriteLine("IMDB Puanı 4-9 arasındakiler:");
+
+        foreach (Filmler film in list.Where(x => x.IMDB > 4 && x.IMDB < 9))
+        {
+            Console.WriteLine($"Filmin adı: {film.Ad} IMDB Puanı: {film.IMDB}");
+
+        }
+        Console.WriteLine("---------------------------");
+        Console.WriteLine("Film adı A ile başlayanlar:");
+
+        foreach (Filmler film in list.Where(x => x.Ad.StartsWith('a') == true || x.Ad.StartsWith('A') == true))
+        {
+            Console.WriteLine($"Filmin adı: {film.Ad} IMDB Puanı: {film.IMDB}");
+
+        }
+
+        Console.ReadKey();
+        return;
+    }
+
+
+}
+
+
+
